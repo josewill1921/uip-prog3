8a9996538d752f0868d15b3cd2dd1db199bc62f1
Emmanuel de Gracia <josewill121@gmail.com>
2015-06-16 15:13:59 -0500

    Quiz5

---
+++ Laboratorios/sem5/quiz5.py
@@ -0,0 +1,6 @@
+>>> Mercados =  ("Supermercado S", "ABC", "DJT",)
+>>> print ("Los mercados son los siguientes")
+>>> print ( Mercados) 
+>>> print ( Mer= "si desea eliminar digite 1, si desea agregar digite 0")
+>>> leer (Mer)
+>>> 
